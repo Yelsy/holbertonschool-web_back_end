f0ec4b24ad2d7f78d65289dc922894fd8ecff0e3
yelsy <yelsycg17@gmail.com>
2023-10-03 20:47:06 +0000

    task 7

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" function to create tuples with a string 
+and the square of an integer or float value."""
+from typing import Tuple, Union
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """return tuple"""
+    return k, pow(v, 2)
