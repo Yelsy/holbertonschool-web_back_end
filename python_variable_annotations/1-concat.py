1d425d23e496dbb8fb7e42981f531490f7060be2
yelsy <yelsycg17@gmail.com>
2023-10-03 04:35:53 +0000

    task 1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" function concat that takes a string str1 and a string
+str2 as arguments and returns a concatenated string"""
+
+
+def concat(a: str, b: str) -> str:
+    """ returns a concatenated string"""
+    return a + b
