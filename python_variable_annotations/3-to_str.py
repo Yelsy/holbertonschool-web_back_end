99fcab650c921eb33df5e3030d8983179a181b10
yelsy <yelsycg17@gmail.com>
2023-10-03 04:56:10 +0000

    task 3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" function that return a string representation of a float"""
+
+
+def to_str(n: float) -> str:
+    """returns the string representation of the float"""
+    return str(n)
