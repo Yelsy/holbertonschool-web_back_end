c45e2a98d6e4574b4569d7cabc67282b001c1ee7
yelsy <yelsycg17@gmail.com>
2023-10-03 21:08:54 +0000

    task 8

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" function  that takes a float multiplier as argument and
+returns a function that multiplies a float by multiplier"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """"""
+    def multiplier_function(x: float) -> float:
+        """return the multiplier function by multiplier"""
+        return multiplier * x
+
+    return multiplier_function
