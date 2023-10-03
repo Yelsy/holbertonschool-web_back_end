b24b8265bd586ea6bf41650b65522cfbe988d85d
yelsy <yelsycg17@gmail.com>
2023-10-03 05:08:28 +0000

    task 5

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" script to sum a element of list"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Return the sum of the list of floats"""
+    return sum(input_list)
