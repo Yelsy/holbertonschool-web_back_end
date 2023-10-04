7d61211268f255052f15ae860ce6e5ff38c1a5ba
yelsy <yelsycg17@gmail.com>
2023-10-03 20:36:35 -0500

    function that  returns the random delay

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""asynchronous coroutine that waits for 
+   a random delay between 0 and max_delay"""
+import asyncio
+import random
+
+
+async def wait_random(max_delay=10):
+    """returns a random delay between 0 and max_delay"""
+    delay_seconds = random.uniform(0, max_delay)
+    await asyncio.sleep(delay_seconds)
+    return delay_seconds
