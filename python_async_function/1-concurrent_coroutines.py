9b1fa81b164d9f18d2fad8933c1bc7463d7d91e4
yelsy <yelsycg17@gmail.com>
2023-10-03 21:11:29 -0500

    fix pycodestyle

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -6,7 +6,7 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """ function that returns a list of floating random numbers n times 
+    """ function that returns a list of floating random numbers n times
     in ascending order"""
     list_delays = [wait_random(max_delay) for _ in range(n)]
 
