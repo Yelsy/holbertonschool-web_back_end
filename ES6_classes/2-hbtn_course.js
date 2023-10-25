18d178055bfc2f4086db7de1899554e02f9fd09d
yelsy <yelsycg17@gmail.com>
2023-10-24 23:17:25 -0500

    fix: multiple spaces

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -3,7 +3,7 @@ export default class HolbertonCourse {
     // Verify attribute types
     if (typeof name !== 'string') throw TypeError('name must be a string');
     if (typeof length !== 'number') throw TypeError('length must be a number');
-    if (!Array.isArray(students)) throw  TypeError( 'students must be an array');
+    if (!Array.isArray(students)) throw TypeError('students must be an array');
     this._name = name;
     this._length = length;
     this._students = students;
