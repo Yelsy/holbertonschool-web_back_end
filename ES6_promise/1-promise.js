bb9dced5d417d7505a267bfb1cd6a8bc124cdb42
yelsy <yelsycg17@gmail.com>
2023-11-01 20:30:29 -0500

    feat: returns a Promise based on the provided conditions

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -2,16 +2,14 @@ function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     // Resolve the promise with a successful response
     if (success) {
-      resolve ({
+      resolve({
         status: 200,
-        body: 'Success'
+        body: 'Success',
       });
+    } else {
+        reject(new Error('The fake API is not working currently'));
     }
-    // Reject the promise with an error message
-    else {
-      reject(new Error('The fake API is not working currently'));
-    }
-    });
+  });
 }
 
 export default getFullResponseFromAPI;
