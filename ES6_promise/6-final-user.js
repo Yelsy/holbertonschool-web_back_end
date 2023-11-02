3a7fbd7d377792db0b4d8089d0da24021bb55db4
yelsy <yelsycg17@gmail.com>
2023-11-01 22:55:44 -0500

    feat: Handle multiple promises

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,7 +2,7 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return new Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+  return new Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
   .then(([userResult, photoResult]) => {
     return [
       {
