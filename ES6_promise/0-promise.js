1a70cc2f3d6bca2f19510c55ce20a46c8865338d
yelsy <yelsycg17@gmail.com>
2023-11-01 19:45:11 -0500

    fix: Eslint

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,11 +1,7 @@
 function getResponseFromAPI() {
   return new Promise((resolve, reject) => {
-    if (true) {
-      resolve();
-    }
-    else {
-      reject(new Error('error'));
-    }
+    resolve();
+    reject(new Error(Error));
   });
 }
 
