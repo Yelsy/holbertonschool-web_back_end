7e10c0c0d52c614a8c0bf8b494c568a342876054
yelsy <yelsycg17@gmail.com>
2023-11-02 22:46:18 -0500

    feat: return student by city

---
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,5 @@
+export default function getStudentsByLocation(student, city) {
+    const studentByLocation = student.filter(
+      (student) => student.location === city);
+    return studentByLocation;
+}
