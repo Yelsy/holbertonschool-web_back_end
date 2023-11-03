3acfd1c5c14003f76364af0a7a2d4987d8e1595e
yelsy <yelsycg17@gmail.com>
2023-11-03 00:07:19 -0500

    fix: eslint

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,12 +1,12 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
   return students
-    .filter(student => student.location === city)
-    .map(student => {
-      const gradeObj = newGrades.filter(grade => grade.studentId === student.id);
-  
-    if (gradeObj.length > 0) {
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const gradeObj = newGrades.filter((grade) => grade.studentId === student.id);
+
+      if (gradeObj.length > 0) {
         return { ...student, grade: gradeObj[0].grade };
-    } else {
+      } else {
         return { ...student, grade: 'N/A' };
       }
     });
