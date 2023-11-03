8c34475718d9e98b02f8f99a293bd21239723280
yelsy <yelsycg17@gmail.com>
2023-11-03 00:00:58 -0500

    feat: update grade by city

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,13 @@
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  return students
+    .filter(student => student.location === city)
+    .map(student => {
+      const gradeObj = newGrades.filter(grade => grade.studentId === student.id);
+  
+    if (gradeObj.length > 0) {
+        return { ...student, grade: gradeObj[0].grade };
+    } else {
+        return { ...student, grade: 'N/A' };
+      }
+    });
+}
