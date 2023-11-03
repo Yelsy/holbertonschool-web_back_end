dc058284fbbf2bf59a5321d15f21b04632a6233e
yelsy <yelsycg17@gmail.com>
2023-11-02 23:03:55 -0500

    fix: identation

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,6 +1,6 @@
 export default function getStudentsByLocation(student, city) {
   const studentByLocation = student.filter(
     (student) => student.location === city,
-    );
+  );
   return studentByLocation;
 }
