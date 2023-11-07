eb12108fea5698c096659b0d9b7098f6ff0a0bfd
yelsy <yelsycg17@gmail.com>
2023-11-06 21:41:41 -0500

    fix: err string

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,5 @@
 export default function cleanSet(set, startString) {
+  const filteredSet = [];
   if (!startString || !startString.length) return '';
   for (const value of set) {
     if (value && value.startsWith(startString)) filteredSet.push(value.slice(startString.length));
