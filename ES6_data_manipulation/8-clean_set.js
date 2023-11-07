88e6d70eb7ae838046510cff71bad8446fa0ea14
yelsy <yelsycg17@gmail.com>
2023-11-06 21:36:38 -0500

    feat: clean set

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,14 +1,7 @@
 export default function cleanSet(set, startString) {
-  let result = '';
+  if (!string || !string.length) return '';
   for (const value of set) {
-    if (value.startsWith(startString)) {
-      const suffix = value.substring(startString.length);
-      if (result.length > 0) {
-        result += '-';
-      }
-        result += suffix;
-    }
+    if (value && value.startsWith(startString)) filteredSet.push(value.slice(startString.length));
   }
-    
-  return result;
+  return filteredSet.join('-');
 }
