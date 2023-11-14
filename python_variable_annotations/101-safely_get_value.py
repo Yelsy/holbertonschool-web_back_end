ae829de40dd0784e1bf6a1ce12e64efa3aa2bcd8
yelsy <yelsycg17@gmail.com>
2023-11-13 19:08:19 -0500

    feat:More involved type annotations

--- python_variable_annotations/101-safely_get_value.py
+++ python_variable_annotations/101-safely_get_value.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
-"""
-    More involved type annotations
-"""
-from typing import TypeVar, Mapping, Any, Union
+""" More involved type annotations"""
+from typing import Mapping, Any, Union, TypeVar
+
 
 T = TypeVar('T')
 
 
-def safely_get_value(dct: Mapping[Any, T], key: Any, default: Union[T, None] = None) -> Union[T, None]:
-    """return either a value of type T or None."""
+def safely_get_value(dct: Mapping, key: Any,
+                     default: Union[T, None]) -> Union[Any, T]:
+    "Function return a value from a dictionary"
     if key in dct:
         return dct[key]
     else:
