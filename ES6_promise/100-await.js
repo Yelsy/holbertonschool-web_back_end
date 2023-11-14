2217845fdbee7c024a91c3bd22aea36a8101984f
yelsy <yelsycg17@gmail.com>
2023-11-13 19:51:04 -0500

    feat: Return an object with the results

---
+++ ES6_promise/100-await.js
@@ -0,0 +1,19 @@
+// Return an object with the results
+import { uploadPhoto, createUser } from './utils';
+
+const asyncUploadUser = async () => {
+  let photo;
+  let user;
+
+  try {
+    photo = await uploadPhoto();
+    user = await createUser();
+  } catch (e) {
+    photo = null;
+    user = null;
+  }
+
+  return ({ photo, user });
+};
+
+export default asyncUploadUser;
